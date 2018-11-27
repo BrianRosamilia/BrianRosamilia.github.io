65d123fc4cd3333c2d2aae3fa7b6df394c90e69e
DESKTOP-UPMPPPB\brian <brian.rosamilia@gmail.com>
2018-11-27 00:50:56 -0500

    Update information to the latest Turn the volume down on the gradient

--- main.js
+++
@@ -1,9 +0,0 @@
-var quotes = ['hey its me ur brother',
-    'the pain slowly fades, but the love is forever',
-    'i have always been thirsty for knowledge',
-    'far better is it to dare mighty things, to win glorious triumphs, even though checkered by failure... than to rank with those poor spirits who neither enjoy nor suffer much, because they live in a gray twilight that knows not victory nor defeat',
-    'the wind is still rising'];
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('#footer').textContent += "\"" + quotes[Math.floor(Math.random() * quotes.length)] + "\""
-});
